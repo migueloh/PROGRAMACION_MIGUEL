1ac0283a69328d3a28b7873f5144a46bd4e0d853
Miguel Olmo <olmomigueltrabajo@gmail.com>
2017-11-27 12:26:51 +0100

    Ejer 3
    en proceso

--- Tema5_Pract2_Ejer3/src/tema5_pract2_ejer3/Tema5_Pract2_Ejer3.java
+++ Tema5_Pract2_Ejer3/src/tema5_pract2_ejer3/Tema5_Pract2_Ejer3.java
@@ -1,5 +1,5 @@
 /*
-Tenemos una matriz bidimensional de boolean donde cada posicion´
+Tenemos una matriz bidimensional de boolean donde cada posicion
 indica si el punto (x, y) es blanco (true) o negro(false).
 
 Ejemplo:
@@ -17,29 +17,44 @@ El borde superior es el valor maximo de y donde algun punto es TRUE.
 
 El borde inferior es el valor mınimo de y donde algun punto es TRUE. 
 
-El borde izquierdo es el valor mınimo de x donde algun punto es ´TRUE.
+El borde izquierdo es el valor mınimo de x donde algun punto es TRUE.
 
-El borde derecho es el valor maximo de x donde alg un punto es TRUE.
+El borde derecho es el valor maximo de x donde algun punto es TRUE.
  */
 package tema5_pract2_ejer3;
 
+import java.util.Arrays;
+
 /**
- *
  * @author 1gdaw07 MIGUEL OLMO
  */
 public class Tema5_Pract2_Ejer3 {
 
-
     public static void main(String[] args) {
         // INICIO MAIN
-        
+
         boolean xBlanco = true;
         boolean yNegro = false;
+
+        boolean[][] aBidi = new boolean[6][10];
+
+        boolean[][] aBidi2 = {{false, false, false, false, false, false, false, false, false, false}, {false, false, 1, 1, 1, 1, 1, 1, false, false}, {false, false, false, 1, 1, 1, 1, 1, 0, 0}, {0, 0, 0, 0, 1, 1, 1, 1, 0, 0}, {0, 0, 0, 0, 0, 1, 1, 1, 0, 0}, {false, false, false, false, false, false, false, false, false, false}};
+
         
-        boolean[][] arrayBidimensional = new boolean[6][10];
+ 
+        for(int i = 1; i < aBidi2.length; i++) {
+            
+        Arrays.binarySearch(aBidi2, i);
         
+            for (int x = 0; x < aBidi2.length; x++) {
+                
+                Arrays.binarySearch(aBidi2, x);
+            } 
+            
+        }
+
         
         
     }
-    
+
 }
