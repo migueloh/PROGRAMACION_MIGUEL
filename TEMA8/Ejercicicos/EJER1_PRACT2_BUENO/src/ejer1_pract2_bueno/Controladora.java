b50a494c57254846617fd130490e28bb7b90e270
Miguel Olmo <olmomigueltrabajo@gmail.com>
2018-03-20 14:09:24 +0100

    EJER2_PRACT2_BUENO_v3

--- TEMA8/Ejercicicos/EJER1_PRACT2_BUENO/src/ejer1_pract2_bueno/Controladora.java
+++ TEMA8/Ejercicicos/EJER1_PRACT2_BUENO/src/ejer1_pract2_bueno/Controladora.java
@@ -3,6 +3,7 @@ package ejer1_pract2_bueno;
 import Vistas.vAbogadoInsertar;
 import BD.*;
 import UML.Abogado;
+import Vistas.vPrincipal;
 
 /**
  *
@@ -10,25 +11,25 @@ import UML.Abogado;
  */
 public class Controladora {
 
-    private static vAbogadoInsertar vA;
+    private static vPrincipal vP;
     private static AbogadoBD vABD;
 
     public static void main(String[] args) {
 
-        
 // INICIO MAIN
-vABD = new AbogadoBD();
-        abrirVentanaAbogado(vA = new vAbogadoInsertar());
+        vABD = new AbogadoBD();
+
+        abrirVentanaPrincipal(vP = new vPrincipal());
 
     }
 
-    public static void abrirVentanaAbogado(vAbogadoInsertar vAbogado) {
-        vA.setVisible(true);
+    private static void abrirVentanaPrincipal(vPrincipal vPrincipal) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     public static void generarAbogado(String dni, String nombre, String ape1, String ape2, String dir) throws Exception {
         Abogado a = new Abogado(dni, nombre, ape1, ape2, dir);
-        
+
         vABD.insertarBD(a);
     }
 
